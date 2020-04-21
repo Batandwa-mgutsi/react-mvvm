75566eec1969aa2d114c63205dd9df17df573493
Batandwa Mgutsi <B.mgutsi@gmail.com>
2020-04-21 17:20:38 +0200

    Final touches

--- mvvm.js
+++ mvvm.js
@@ -12,10 +12,16 @@ var React = require('react');
  * Views register callbacks to the view model and will be be notified when notifyListeners is called.
  * Generally, only one view should use a viewmodel.
  */
-export default class BaseViewModel {
+export class BaseViewModel {
   constructor() {
     this._listeners = [];
     this._busy = false;
+
+    this.setBusy = this.setBusy.bind(this);
+    this.isBusy = this.isBusy.bind(this);
+    this.subscribe = this.subscribe.bind(this);
+    this.clearSubscriptions = this.clearSubscriptions.bind(this);
+    this.notifyListeners = this.notifyListeners.bind(this);
   }
 
   /**
@@ -37,7 +43,7 @@ export default class BaseViewModel {
   /**
    * Add the given function as callback when notifyListeners is called.
    * 
-   * @param {function()} [callback] Is the function to be add as callback.
+   * @param {function(BaseViewModel)} [callback] Is the function to be add as callback.
    */
   subscribe(callback) {
     this._listeners.push(callback);
@@ -52,10 +58,12 @@ export default class BaseViewModel {
 
   /**
    * Calls all the registered callback functions.
+   * 
+   * @param {BaseViewModel} thiz is the viewmodel to pass to listeners.
    */
-  notifyListeners() {
+  notifyListeners(thiz) {
     for (var iii = 0; iii < this._listeners.length; iii++)
-      this._listeners[iii]();
+      this._listeners[iii](thiz);
   }
 }
 
@@ -63,7 +71,7 @@ export default class BaseViewModel {
 /**
  * A react component that subscribes to the given view model.
  */
-export default class ViewModelConsumer extends React.Component {
+export class ViewModelConsumer extends React.Component {
 
   /**
    * @param {BaseViewModel} viewModel is the view model to subscribe to.
@@ -72,13 +80,17 @@ export default class ViewModelConsumer extends React.Component {
     super(props);
     this.state = viewModel;
 
-    this.state.subscribe(() => {
-      this.setState(this.state);
+    this.state.subscribe((model) => {
+      this.setState(model);
     });
   }
 
   componentDidMount() {
     this.onModelReady();
+  }
+
+  componentWillUnmount() {
+    this.state.clearSubscriptions();
   }
 
   render() {
@@ -100,6 +112,6 @@ export default class ViewModelConsumer extends React.Component {
    * @param {BaseViewModel} viewModel is the viewModel for this component.
    */
   onRender(props, viewModel) {
-    return <h1 style="color:red">This Component does not implement <code>onRender(props, viewModel)</code></h1>;
+    return <h1 style={{ color: 'red' }}>This Component does not implement <code>onRender(props, viewModel)</code></h1>;
   }
 }
